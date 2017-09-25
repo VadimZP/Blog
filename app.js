016caf247df366981edf2ad33a169995d2c23a07
vadim <wesborlandzp@gmail.com>
2017-09-25 19:04:15 +0300

    ---

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = (function () {
 
     /** Variables */
-    const addPostBtn = document.getElementById('btn_add-post');
     let postId = 0;
 
     /**
@@ -18,19 +17,19 @@ const app = (function () {
          * Output posts to the page from storage.
          * @param  {boolean} addNewPost - if true, add newly created post
          */
-        this.renderPosts = function (addNewPost) {
+        this.appendPosts = function (addNewPost) {
             if (!this.posts.length) return;
 
             const postsContainer = document.querySelector('.post-list');
 
             if (addNewPost) {
                 const addedPost = this.posts[this.posts.length - 1];
-                const postElem = addedPost.createPostStruct(addedPost.text);
+                const postElem = addedPost.postView(addedPost.text);
 
                 postsContainer.appendChild(postElem);
             } else {
                 this.posts.forEach(elem => {
-                    const postElem = elem.createPostStruct(elem.text);
+                    const postElem = elem.postView();
 
                     postsContainer.appendChild(postElem);
                 });
@@ -40,72 +39,79 @@ const app = (function () {
         /**
          * Adds new post-object to the post's storage.
          */
-        this.addPost = () => {
-            function Post(id) {
-                this.id = id;
-            }
+        this.addPost = function () {
+            _.posts.push(new Post(postId++));
 
-            /**
-             * Creates post HTML structure.
-             * @param  {string} postText - text property from post-object
-             */
-            Post.prototype.createPostStruct = function () {
-
-                /** Variables */
-                let postDocFrag, li, header, body, btnRemove;
-
-                postDocFrag = document.createDocumentFragment();
-
-                li = document.createElement('li');
-                li.className = 'post';
-
-                header = document.createElement('header');
-                header.className = 'post-header';
-
-                btnRemove = document.createElement('button');
-                btnRemove.className = 'btn_remove-post';
-                btnRemove.innerHTML = 'X';
-
-                body = document.createElement('div');
-                body.className = 'post-body';
-                body.innerHTML = this.id;
-
-                li.appendChild(header);
-                li.appendChild(btnRemove);
-                li.appendChild(body);
-
-                postDocFrag.appendChild(li);
-
-                btnRemove.addEventListener('click', () => {
-                    let index = _.posts.findIndex((elem) => {
-                        if (elem.id === this.id) return elem;
-                    });
-                    
-                    _.posts.splice(index, 1);
-
-                    li.remove();
-                });
-                
-                return postDocFrag;
-            }
-
-            this.posts.push(new Post(postId++));
-
-            this.renderPosts(true);
+            _.appendPosts(true);
         }
 
+        this.setEventHandlers = function () {
+            this.addPostBtn = document.getElementById('btn_add-post');
 
+            this.addPostBtn.addEventListener('click', this.addPost);
+        }
+
+    }
+
+    function Post(id) {
+        this.id = id;
+        this.heading = 'heading';
+        this.text = 'text';
+    }
+
+    /**
+     * Creates post HTML structure.
+     * @param  {string} postText - text property from post-object
+     */
+    Post.prototype.postView = function () {
+
+        /** Variables */
+        let postDocFrag, li, header, body, btnRemove;
+
+        postDocFrag = document.createDocumentFragment();
+
+        li = document.createElement('li');
+        li.className = 'post';
+
+        header = document.createElement('header');
+        header.className = 'post-header';
+        header.innerHTML = this.heading;
+
+        btnRemove = document.createElement('button');
+        btnRemove.className = 'btn_remove-post';
+        btnRemove.innerHTML = 'X';
+
+        body = document.createElement('div');
+        body.className = 'post-body';
+        body.innerHTML = this.text;
+
+        li.appendChild(header);
+        header.appendChild(btnRemove);
+        li.appendChild(body);
+
+        postDocFrag.appendChild(li);
+
+        btnRemove.addEventListener('click', () => {
+            let result = blog.posts.findIndex((elem) => {
+                if (elem.id === this.id) return elem;
+            });
+            blog.posts.splice(result, 1);
+            li.remove();
+
+        });
+
+        return postDocFrag;
     }
 
     // Initialization step
     const blog = new Blog();
 
     const postAdding = function () {
-        addPostBtn.addEventListener('click', blog.addPost);
+        blog.setEventHandlers();
     }
 
     const postRendering = function () {
-        blog.renderPosts();
+        blog.appendPosts();
     }
 
     const init = function () {
