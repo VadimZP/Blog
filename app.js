f55903fa90061ce0fb54b309e66dde97844a770c
vadim <wesborlandzp@gmail.com>
2017-09-26 16:26:50 +0300

    modal window was added

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const app = (function () {
      * @constructor
      */
     function Blog() {
-        const _ = this;
-
         /**
          * Output posts to the page from storage.
          * @param  {boolean} addNewPost - if true, add newly created post
@@ -25,7 +23,7 @@ const app = (function () {
 
             if (addNewPost) {
                 const addedPost = data[0].posts[data[0].posts.length - 1];
-                const postElem = addedPost.postView(addedPost.text);
+                const postElem = addedPost.postView();
 
                 postsContainer.appendChild(postElem);
             } else {
@@ -41,10 +39,11 @@ const app = (function () {
          * Adds new post-object to the post's storage.
          */
         this.addPost = function () {
+            modal();
             data[0].posts.push(new Post(postId++));
 
-            _.appendPosts(true);
-        }
+            this.appendPosts(true);
+        }.bind(this);
 
         this.addPostBtn = document.getElementById('btn_add-post');
 
@@ -63,9 +62,9 @@ const app = (function () {
      */
     Post.prototype.postView = function () {
 
-        let postDocFrag, li, header, body, btnRemove;
+        let postFrag, li, header, body, btnRemove;
 
-        postDocFrag = document.createDocumentFragment();
+        postFrag = document.createDocumentFragment();
 
         li = document.createElement('li');
         li.className = 'post';
@@ -86,7 +85,7 @@ const app = (function () {
         header.appendChild(btnRemove);
         li.appendChild(body);
 
-        postDocFrag.appendChild(li);
+        postFrag.appendChild(li);
 
         btnRemove.addEventListener('click', () => {
             let postIndex = data[0].posts.findIndex((elem) => {
@@ -96,10 +95,48 @@ const app = (function () {
             data[0].posts.splice(postIndex, 1);
 
             li.remove();
-
         });
 
-        return postDocFrag;
+        return postFrag;
+    }
+
+    function modal() {
+        let modalFrag, overlay, modalWindow, modalHeader, modalBody, btnClose, btnAccept, body;
+
+        body = document.body;
+
+        modalFrag = document.createDocumentFragment();
+
+        overlay = document.createElement('div');
+        overlay.className = 'overlay';
+
+        modalWindow = document.createElement('div');
+        modalWindow.className = 'modal-window';
+
+        modalHeader = document.createElement('div');
+        modalHeader.className = 'modal-header';
+
+        modalBody = document.createElement('div');
+        modalBody.className = 'modal-body';
+
+        btnClose = document.createElement('button');
+        btnClose.className = 'btn_close-modal';
+        btnClose.innerHTML = 'X';
+
+        btnAccept = document.createElement('button');
+        btnAccept.className = 'btn_accept';
+        btnAccept.innerHTML = 'Accept';
+
+        overlay.appendChild(modalWindow);
+        modalWindow.appendChild(modalHeader);
+        modalWindow.appendChild(modalBody);
+        modalWindow.appendChild(btnAccept);
+        modalHeader.appendChild(btnClose);
+
+        modalFrag.appendChild(overlay);
+        
+        body.insertBefore(modalFrag, body.firstChild);
+
     }
 
     // Initialization step
